c3a16a4f178f7313d77e8107e3b3701551431757
Aibala111 <aibalanurbekow001@gmail.com>
2023-01-18 19:19:17 +0600

    First

--- homework_4-1/setting/views.py
+++ homework_4-1/setting/views.py
@@ -2,12 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import datetime
 
-# def time(request):
-#     time = datetime.datetime.now()
-#     return HttpResponse("time")
 def time(request):
-    dt_now = datetime.datetime.now()
-    return HttpResponse(dt_now)
+    time = datetime.datetime.now()
+    return HttpResponse("time")
+# def time(request):
+#     dt_now = datetime.datetime.now()
+#     return HttpResponse(dt_now)
 def goodbye(request):
     return HttpResponse("goodbye")
 
