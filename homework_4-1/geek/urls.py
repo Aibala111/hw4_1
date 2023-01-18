6fae22ec7b1024366119c8384543e4a63ca1e3a7
Aibala111 <aibalanurbekow001@gmail.com>
2023-01-18 18:57:47 +0600

    first commit

--- homework_4-1/geek/urls.py
+++ homework_4-1/geek/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
-
+from setting.views import goodbye,time
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('goodbye/',goodbye),
+    path('time/',time),
 ]
--- homework_4-1/setting/views.py
+++ homework_4-1/setting/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
-# Create your views here.
+# def time(request):
+#     time = datetime.datetime.now()
+#     return HttpResponse("time")
+def time(request):
+    dt_now = datetime.datetime.now()
+    return HttpResponse(dt_now)
+def goodbye(request):
+    return HttpResponse("goodbye")
+
